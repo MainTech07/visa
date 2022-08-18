040d39b645a974f19f5f02e40f59fcf3cfc36f20
MainTech07 <mayonator7@yahoo.com>
2022-08-18 21:10:09 +0000

    tech team updates

--- tech.py
+++ tech.py
@@ -1,4 +1,6 @@
 # Deploying a python web application
-#echo "Hello World"   = shell scripts    
+#echo "Hello World"  
+#= shell scripts    
 print('mims shark') 
 #= python script
+print ("the tech team is wonderful")
