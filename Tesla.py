14f5358180a86d69f242386d203db5efe71ec987
Mayowa Oladipo <mayonator7@yahoo.com>
2022-08-19 16:20:42 +0100

    MIMSHARK Github

--- Tesla.py
+++ Tesla.py
@@ -1,1 +1,9 @@
 # Operational tools for organization
+1. Create Folder where you keep all the scripts for your organization
+2. git init
+3. create script
+4. git add script
+5. commit script
+6. configure global user
+7. git remote add vs "organization repository link"... If not add
+8. push vs master
