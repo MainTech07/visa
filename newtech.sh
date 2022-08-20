cbdccfa9deeb7e6b42aa5399792fbd976d5c5f20
suhaad79 <111306108+suhaad79@users.noreply.github.com>
2022-08-20 15:03:07 +0200

    Update newtech.sh

--- newtech.sh
+++ newtech.sh
@@ -1,9 +1,13 @@
 #this is to allow other developers read my script
 #allow these user work on my file"
-echo "type in your username"
+echo "Create a username"
 read username
-echo "type your password"
+echo "Enter a password"
 read -s password
+echo "Enter your email address"
+read email
+echo "$username, you are welcome to our team"
+echo "A confirmation email with your user details will be sent to $email shortly "
 sudo adduser $username
 echo "password" | sudo passwd "$username" --stdin 
 #echo "you are allowed to work on this file
